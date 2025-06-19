291feaa09c1385b7d5b937be06a611e1506582c5
akiraonstarknet <zkdice@proton.me>
2025-06-19 11:42:49 +0530

    update prisma paths [2]

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,4 +1,4 @@
-export { Prisma, PrismaClient, deposits} from '@prisma/my-client';
+export { Prisma, PrismaClient, deposits} from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
